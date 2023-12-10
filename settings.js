acc6094288cd73d1d6ffaa559b7779a10cfe3e91
sensei-ofc <153446153+sensei-ofc@users.noreply.github.com>
2023-12-10 11:02:29 -0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,39 +1,41 @@
-const fs = require('fs')
+const fs = require('fs');
 
-global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
-global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
-global.limitCount = 10000
-global.YUOR_PORT = 8000
+global.creator = 'Zeeoneofc'; // tu nombre
+global.MONGO_DB_URI = ""; // base de datos MongoDB
+global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10"; // cualquier valor que desees
+global.your_email = ""; // correo electrónico
+global.email_password = ""; // contraseña de la aplicación del correo electrónico
+global.limitCount = 10000;
+global.YOUR_PORT = 8000;
 global.loghandler = {
-	noapikey:{
-		status: 403,
-        message: 'Input parameter apikey',
+    noapikey: {
+        status: 403,
+        message: 'Parámetro de entrada apikey',
         creator: `${creator}`,
         result: "error"
     },
     error: {
         status: 503,
-        message: 'Service Unavaible, Sedang dalam perbaikan',
+        message: 'Servicio no disponible, en reparación',
         creator: `${creator}`
     },
     apikey: {
-    	status: 403,
-    	message: 'Forbiden, Invalid apikey',
-    	creator: `${creator}`
+        status: 403,
+        message: 'Prohibido, apikey no válido',
+        creator: `${creator}`
     },
     noturl: {
-    	status: 403,
-    	message: 'Forbiden, Invlid url, masukkan parameter url',
-    	creator: `${creator}`,
+        status: 403,
+        message: 'Prohibido, URL no válida, ingrese el parámetro de URL',
+        creator: `${creator}`,
     }
-}
-let file = require.resolve(__filename)
+};
+
+let file = require.resolve(__filename);
+
 fs.watchFile(file, () => {
-	fs.unwatchFile(file)
-	console.log(`Update'${__filename}'`)
-	delete require.cache[file]
-	require(file)
-})
+    fs.unwatchFile(file);
+    console.log(`Actualizado '${__filename}'`);
+    delete require.cache[file];
+    require(file);
+});
