08aa80640e87d6dea409391ef154bc49e7191036
sensei-ofc <153446153+sensei-ofc@users.noreply.github.com>
2023-12-10 17:25:33 -0500

    Update apikey.js

--- assets/js/plugins/apikey.js
+++ assets/js/plugins/apikey.js
@@ -1,37 +1,36 @@
+const textnya = document.querySelector('.status');
+const warn = document.querySelector('.warn');
+var apikey = document.getElementById("responapikeymucoooookkk")
+const amlert = document.querySelector('.alert-danger');
+apikey.addEventListener("keyup", function (event) {
+if (event.key == 'Enter') {
+    event.preventDefault();
+    document.getElementById("diklikcok").click();
+  }
+});
 
-    const textnya = document.querySelector('.status');
-    const warn = document.querySelector('.warn');
-    var apikey = document.getElementById("responapikeymucoooookkk")
-    const amlert = document.querySelector('.alert-danger');
-    apikey.addEventListener("keyup", function (event) {
-    if (event.key == 'Enter') {
-        event.preventDefault();
-        document.getElementById("diklikcok").click();
-      }
-    });
+function enviarDatos() {
+  if (apikey.value == '') {
+    alert("¡Introduce tu apikey!", "", "error");
+  } else {
+    var xhr = new XMLHttpRequest();
+    var url = `/api/cekapikey?apikey=${apikey.value}`;
+    xhr.onloadend = function () {
 
-    function sendata() {
-      if (apikey.value == '') {
-        alert("Masukkan apikeymu!", "", "error");
+      data = JSON.parse(this.responseText);
+      if (data.status == true) {
+        textnya.innerHTML = '[ INFO ] El estado de tu Apikey es Activo <br><br>' + 'Ip: ' + data.response.ip + '<br>------------------------<br>' + 'nombre: ' + data.response.name + '<br>------------------------<br>' + 'email: ' + data.response.email + '<br>------------------------<br>' + 'apikey: ' + data.response.apikey + '<br>------------------------<br>' + 'totalhit: ' + data.response.totalhit + '<br>------------------------<br>' + 'premium: ' + data.response.premium + '<br>------------------------<br>' + 'expirado: ' + data.response.expired 
+
+        swal("¡Apikey válida!", 'El estado de tu Apikey es Activo', "success");
+        warn.textContent = ''
       } else {
-        var xhr = new XMLHttpRequest();
-        var url = `/api/cekapikey?apikey=${apikey.value}`;
-        xhr.onloadend = function () {
-
-          data = JSON.parse(this.responseText);
-          if (data.status == true) {
-            textnya.innerHTML = '[ INFO ] Status Apikey anda adalah Active <br><br>' + 'Ip: ' + data.response.ip + '<br>------------------------<br>' + 'nama: ' + data.response.name + '<br>------------------------<br>' + 'email: ' + data.response.email + '<br>------------------------<br>' + 'apikey: ' + data.response.apikey + '<br>------------------------<br>' + 'totalhit: ' + data.response.totalhit + '<br>------------------------<br>' + 'premium: ' + data.response.premium + '<br>------------------------<br>' + 'expired: ' + data.response.expired 
-
-            swal("Apikey valid!", 'Status Apikey anda adalah Active', "success");
-            warn.textContent = ''
-          } else {
-            textnya.textContent = '[ INFO ] Apikey anda tidak valid!'
-            swal("Apikey Tidak Valid!", "", "error");
-            alert('Apikey Tidak Valid. Silahkan beli apikey ke Instagram @zeeoneofc');
-            amlert.classList.remove('hideop')
-          }
-        };
-        xhr.open("GET", url, true);
-        xhr.send();
+        textnya.textContent = '[ INFO ] ¡Tu Apikey no es válida!'
+        swal("¡Apikey no válida!", "", "error");
+        alert('Apikey no válida. Por favor, compra una apikey en Instagram @zeeoneofc');
+        amlert.classList.remove('hideop')
       }
-    } 
+    };
+    xhr.open("GET", url, true);
+    xhr.send();
+  }
+}
