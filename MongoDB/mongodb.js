f0a954e27d8db5e02adb27d35d422746e5e08136
sensei-ofc <153446153+sensei-ofc@users.noreply.github.com>
2023-12-10 17:22:57 -0500

    Update mongodb.js

--- MongoDB/mongodb.js
+++ MongoDB/mongodb.js
@@ -1,16 +1,16 @@
 require('../settings');
 const mongoose = require('mongoose');
 
-function connectMongoDb() {
+function conectarMongoDb() {
     mongoose.connect(MONGO_DB_URI, { 
       useNewUrlParser: true, 
       useUnifiedTopology: true
     });
     const db = mongoose.connection;
-    db.on('error', console.error.bind(console, 'connection error:'));
+    db.on('error', console.error.bind(console, 'error de conexión:'));
     db.once('open', () => {
-      console.log('Succes connect to MONGODB ✅');
+      console.log('Conexión exitosa a MONGODB ✅');
     });
 };
 
-module.exports.connectMongoDb = connectMongoDb;
+module.exports.conectarMongoDb = conectarMongoDb;
